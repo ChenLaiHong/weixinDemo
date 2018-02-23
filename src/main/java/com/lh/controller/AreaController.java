98b88a055d9f224c5d8870eb8403f67a3af6484d
ChenLaiHong <1185630400@qq.com>
2018-02-23 22:56:47 +0800

    controller

---
+++ src/main/java/com/lh/controller/AreaController.java
@@ -0,0 +1,57 @@
+package com.lh.controller;
+
+import com.lh.pojo.Area;
+import com.lh.service.AreaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by CHLaih on 2018/2/23.
+ */
+@RestController
+@RequestMapping("/superadmin")
+public class AreaController {
+    @Autowired
+    private AreaService areaService;
+
+    @RequestMapping(value = "/listarea",method = RequestMethod.GET)
+    private Map<String,Object> listArea(){
+        Map<String,Object> modelMap = new HashMap<String,Object>();
+        List<Area> list = areaService.queryArea();
+        modelMap.put("areaList",list);
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/getareabyid",method = RequestMethod.GET)
+    private Map<String,Object> getAreaById(Integer areaId){
+        Map<String,Object> modelMap = new HashMap<String,Object>();
+        Area area = areaService.getAreaById(areaId);
+        modelMap.put("area",area);
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/addarea",method = RequestMethod.POST)
+    private Map<String,Object> addArea(@RequestBody Area area){
+        Map<String,Object> modelMap = new HashMap<String,Object>();
+        modelMap.put("success",areaService.addArea(area));
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/modifyarea",method = RequestMethod.POST)
+    private Map<String,Object> modifyArea(@RequestBody Area area){
+        Map<String,Object> modelMap = new HashMap<String,Object>();
+        modelMap.put("success",areaService.modifyArea(area));
+        return modelMap;
+    }
+
+    @RequestMapping(value = "/removearea",method = RequestMethod.GET)
+    private Map<String,Object> removeArea(Integer areaId){
+        Map<String,Object> modelMap = new HashMap<String,Object>();
+        modelMap.put("success",areaService.deleteArea(areaId));
+        return modelMap;
+    }
+}
